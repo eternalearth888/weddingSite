dfc821142e0d02eb18b9e5a3c340edb06a4eb887
eternalearth888 <mdeslis@gmail.com>
2015-11-03 22:17:01 -0700

    working localhost

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   </div>
   <div class="mdl-card__supporting-text">
     <div id="main_subject">ARE GETTING MARRIED!!!</div>
-	<div id="main_date">May 24, 2016</div>
+	<div id="main_date">May 22, 2016</div>
   </div>
   <div class="mdl-card__actions mdl-card--border">
 		<a href = "./story.php" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
