04443f4f32d842481bc2abb9dc2e1c12037d1967
eternalearth888 <mdeslis@gmail.com>
2015-11-08 20:05:18 -0700

    linux update

--- contact.php
+++ contact.php
@@ -1,3 +1,26 @@
 <?php include('includes/meta.php'); ?>
-	Contact Us
+<div class="demo-card-wide-reg mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title">
+    <h2 class="mdl-card__title-text">Contact Us</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+	<ul class="collection">
+    <li class="collection-item avatar">
+      <img src="images/nikki.png" alt="" class="circle">
+      <span class="title">Nikki Montoya</span>
+	<p>
+	<a href="https://www.facebook.com/nikki.montoya.16?fref=ts" target="_blank">Facebook</a>
+	</p>
+    </li>
+        <li class="collection-item avatar">
+      <img src="images/joe.png" alt="" class="circle">
+      <span class="title">Joseph Tucker</span>
+	<p>
+	<a href="https://www.facebook.com/jtucker1210" target="_blank">Facebook</a>
+	<br />
+	</p>
+    </li>
+  </ul>
+  </div>
+</div>
 <?php include('includes/end.php'); ?>
