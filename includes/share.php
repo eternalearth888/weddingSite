108d5260948a36b0632c6d9b0a89be6953319c02
eternalearth888 <mdeslis@gmail.com>
2016-02-28 17:29:37 -0700

    fixed registries photo and share button on main page

--- includes/share.php
+++ includes/share.php
@@ -1,11 +1,26 @@
-<div class="mdl-card__menu">
+<div class="mdl-card__menu"  width="30px" >
     <button id="demo-menu-lower-right" class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-      <i class="material-icons">share</i>
+      <i class="material-icons" >share</i>
     </button>
     
     <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
     for="demo-menu-lower-right">
-      <li class="mdl-menu__item">Gmail</li>
-      <li class="mdl-menu__item">Facebook</li>
+      <li  class="mdl-menu__item">
+      	<!-- Facebook -->
+    <a href="http://www.facebook.com/sharer.php?u=http://www.joeandnikkiweddingday.com/" target="_blank">
+        Facebook
+    </a>
+</li>
+      <li class="mdl-menu__item"> <!-- Google+ -->
+	    <a href="https://plus.google.com/share?url=http://www.joeandnikkiweddingday.com/" target="_blank">
+	        Google+
+	    </a>
+	</li>
+      <li class="mdl-menu__item">
+ <!-- Email -->
+    <a href="mailto:?Subject=Joe And Nikki Wedding Day&amp;Body=Type%20your%20message%20here">
+        Email
+    </a>
+      </li>
     </ul>
   </div>
